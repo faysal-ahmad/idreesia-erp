0c87a1536e6273cec463c0db3b4f84946fb71ae6
Faisal Ahmad <faisal.idreesi@gmail.com>
2018-07-29 19:27:46 +0500

    Use graphql server url from settings.json instead of hardcoding.

--- client/main.js
+++ client/main.js
@@ -4,11 +4,11 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import { ApolloProvider } from 'react-apollo';
-import { ApolloLink, from } from "apollo-link";
+import { ApolloLink, from } from 'apollo-link';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { onError } from "apollo-link-error";
+import { onError } from 'apollo-link-error';
 
 import '../node_modules/antd/dist/antd.css';
 
@@ -16,16 +16,15 @@ import App from '../imports/ui/app';
 
 const cache = new InMemoryCache();
 const httpLink = new HttpLink({
-  // uri: Meteor.absoluteUrl('graphql'),
-  uri: 'http://192.168.1.4:4000/graphql',
+  uri: Meteor.settings.public.graphqlServerUrl,
 });
 
 const authLink = new ApolloLink((operation, forward) => {
   const token = Accounts._storedLoginToken();
   operation.setContext(() => ({
     headers: {
-      "meteor-login-token": token
-    }
+      'meteor-login-token': token,
+    },
   }));
   return forward(operation);
 });
@@ -33,11 +32,13 @@ const authLink = new ApolloLink((operation, forward) => {
 const errorLink = onError(({ graphQLErrors, networkError }) => {
   if (graphQLErrors)
     graphQLErrors.map(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-      )
+      // eslint-disable-next-line no-console
+      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
     );
-  if (networkError) console.log(`[Network error]: ${networkError}`);
+  if (networkError) {
+    // eslint-disable-next-line no-console
+    console.log(`[Network error]: ${networkError}`);
+  }
 });
 
 const client = new ApolloClient({

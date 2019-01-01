df43c4841e1609411d87071b6dcfa601b03e7cd1
Faisal Ahmad <faisal.idreesi@gmail.com>
2019-01-01 12:33:11 +0500

    Removed files related to return forms.

--- imports/lib/constants/permissions.js
+++ imports/lib/constants/permissions.js
@@ -2,39 +2,35 @@ export default class Permissions {
   // ****************************************************************************************
   // Admin Module
   // ****************************************************************************************
-  static ADMIN_VIEW_ACCOUNTS = 'admin-view-accounts';
-  static ADMIN_MANAGE_ACCOUNTS = 'admin-manage-accounts';
+  static ADMIN_VIEW_ACCOUNTS = "admin-view-accounts";
+  static ADMIN_MANAGE_ACCOUNTS = "admin-manage-accounts";
 
-  static ADMIN_VIEW_PHYSICAL_STORES = 'admin-view-physical-stores';
-  static ADMIN_MANAGE_PHYSICAL_STORES = 'admin-manage-physical-stores';
+  static ADMIN_VIEW_PHYSICAL_STORES = "admin-view-physical-stores";
+  static ADMIN_MANAGE_PHYSICAL_STORES = "admin-manage-physical-stores";
 
-  static ADMIN_VIEW_FINANCIAL_ACCOUNTS = 'admin-view-financial-accounts';
-  static ADMIN_MANAGE_FINANCIAL_ACCOUNTS = 'admin-manage-financial-accounts';
+  static ADMIN_VIEW_FINANCIAL_ACCOUNTS = "admin-view-financial-accounts";
+  static ADMIN_MANAGE_FINANCIAL_ACCOUNTS = "admin-manage-financial-accounts";
 
   // ****************************************************************************************
   // HR Module
   // ****************************************************************************************
-  static HR_MANAGE_SETUP_DATA = 'hr-manage-setup-data';
-  static HR_VIEW_KARKUNS = 'hr-view-karkuns';
-  static HR_MANAGE_KARKUNS = 'hr-manage-karkuns';
-  static HR_MANAGE_KARKUN_DUTIES = 'hr-manage-karkun-duties';
+  static HR_MANAGE_SETUP_DATA = "hr-manage-setup-data";
+  static HR_VIEW_KARKUNS = "hr-view-karkuns";
+  static HR_MANAGE_KARKUNS = "hr-manage-karkuns";
+  static HR_MANAGE_KARKUN_DUTIES = "hr-manage-karkun-duties";
 
   // ****************************************************************************************
   // Inventory Module
   // ****************************************************************************************
-  static IN_MANAGE_SETUP_DATA = 'inventory-manage-setup-data';
-  static IN_VIEW_STOCK_ITEMS = 'inventory-view-stock-items';
-  static IN_MANAGE_STOCK_ITEMS = 'inventory-manage-stock-items';
+  static IN_MANAGE_SETUP_DATA = "inventory-manage-setup-data";
+  static IN_VIEW_STOCK_ITEMS = "inventory-view-stock-items";
+  static IN_MANAGE_STOCK_ITEMS = "inventory-manage-stock-items";
 
-  static IN_VIEW_ISSUANCE_FORMS = 'inventory-view-issuance-forms';
-  static IN_MANAGE_ISSUANCE_FORMS = 'inventory-manage-issuance-forms';
-  static IN_APPROVE_ISSUANCE_FORMS = 'inventory-approve-issuance-forms';
+  static IN_VIEW_ISSUANCE_FORMS = "inventory-view-issuance-forms";
+  static IN_MANAGE_ISSUANCE_FORMS = "inventory-manage-issuance-forms";
+  static IN_APPROVE_ISSUANCE_FORMS = "inventory-approve-issuance-forms";
 
-  static IN_VIEW_RETURN_FORMS = 'inventory-view-return-forms';
-  static IN_MANAGE_RETURN_FORMS = 'inventory-manage-return-forms';
-  static IN_APPROVE_RETURN_FORMS = 'inventory-approve-return-forms';
-
-  static IN_VIEW_PURCHASE_FORMS = 'inventory-view-purchase-forms';
-  static IN_MANAGE_PURCHASE_FORMS = 'inventory-manage-purchase-forms';
-  static IN_APPROVE_PURCHASE_FORMS = 'inventory-approve-purchase-forms';
+  static IN_VIEW_PURCHASE_FORMS = "inventory-view-purchase-forms";
+  static IN_MANAGE_PURCHASE_FORMS = "inventory-manage-purchase-forms";
+  static IN_APPROVE_PURCHASE_FORMS = "inventory-approve-purchase-forms";
 }
--- imports/lib/schemas/inventory/index.js
+++ imports/lib/schemas/inventory/index.js
@@ -1,8 +1,7 @@
-export { default as IssuanceForm } from './issuance-form';
-export { default as ItemCategory } from './item-category';
-export { default as ItemType } from './item-type';
-export { default as PhysicalStore } from './physical-store';
-export { default as ReturnForm } from './return-form';
-export { default as PurchaseForm } from './purchase-form';
-export { default as StockItem } from './stock-item';
-export { default as StockItemAdjustment } from './stock-item-adjustment';
+export { default as IssuanceForm } from "./issuance-form";
+export { default as ItemCategory } from "./item-category";
+export { default as ItemType } from "./item-type";
+export { default as PhysicalStore } from "./physical-store";
+export { default as PurchaseForm } from "./purchase-form";
+export { default as StockItem } from "./stock-item";
+export { default as StockItemAdjustment } from "./stock-item-adjustment";

1c1c856529f187f2f4d3bc9fce6856d31c577978
Faisal Ahmad <faisal.idreesi@gmail.com>
2019-01-01 12:41:52 +0500

    Removed schema references for return forms

--- imports/api/index.js
+++ imports/api/index.js
@@ -1,58 +1,55 @@
-import { merge } from 'lodash';
+import { merge } from "lodash";
 
 /**
  * Common Schema and Resolvers
  */
-import AttachmentSchema from './common/attachments/attachment.graphql';
-import AttachmentResolvers from './common/attachments/resolvers';
+import AttachmentSchema from "./common/attachments/attachment.graphql";
+import AttachmentResolvers from "./common/attachments/resolvers";
 
 /**
  * Schema and Resolvers for the HR module
  */
-import DutyLocationSchema from './hr/duty-location/duty-location.graphql';
-import DutyLocationResolvers from './hr/duty-location/resolvers';
+import DutyLocationSchema from "./hr/duty-location/duty-location.graphql";
+import DutyLocationResolvers from "./hr/duty-location/resolvers";
 
-import DutySchema from './hr/duty/duty.graphql';
-import DutyResolvers from './hr/duty/resolvers';
+import DutySchema from "./hr/duty/duty.graphql";
+import DutyResolvers from "./hr/duty/resolvers";
 
-import KarkunSchema from './hr/karkuns/karkun.graphql';
-import KarkunResolvers from './hr/karkuns/resolvers';
+import KarkunSchema from "./hr/karkuns/karkun.graphql";
+import KarkunResolvers from "./hr/karkuns/resolvers";
 
-import KarkunDutySchema from './hr/karkun-duties/karkun-duty.graphql';
-import KarkunDutyResolvers from './hr/karkun-duties/resolvers';
+import KarkunDutySchema from "./hr/karkun-duties/karkun-duty.graphql";
+import KarkunDutyResolvers from "./hr/karkun-duties/resolvers";
 
 /**
  * Schema and Resolvers for the Inventory module
  */
-import CommonSchema from './inventory/common/common.graphql';
-import CommonResolvers from './inventory/common/resolvers';
+import CommonSchema from "./inventory/common/common.graphql";
+import CommonResolvers from "./inventory/common/resolvers";
 
-import ItemCategorySchema from './inventory/item-category/item-category.graphql';
-import ItemCategoryResolvers from './inventory/item-category/resolvers';
+import ItemCategorySchema from "./inventory/item-category/item-category.graphql";
+import ItemCategoryResolvers from "./inventory/item-category/resolvers";
 
-import ItemTypeSchema from './inventory/item-type/item-type.graphql';
-import ItemTypeResolvers from './inventory/item-type/resolvers';
+import ItemTypeSchema from "./inventory/item-type/item-type.graphql";
+import ItemTypeResolvers from "./inventory/item-type/resolvers";
 
-import PhysicalStoreSchema from './inventory/physical-store/physical-store.graphql';
-import PhysicalStoreResolvers from './inventory/physical-store/resolvers';
+import PhysicalStoreSchema from "./inventory/physical-store/physical-store.graphql";
+import PhysicalStoreResolvers from "./inventory/physical-store/resolvers";
 
-import StockItemSchema from './inventory/stock-item/stock-item.graphql';
-import StockItemResolvers from './inventory/stock-item/resolvers';
+import StockItemSchema from "./inventory/stock-item/stock-item.graphql";
+import StockItemResolvers from "./inventory/stock-item/resolvers";
 
-import IssuanceFormSchema from './inventory/issuance-form/issuance-form.graphql';
-import IssuanceFormResolvers from './inventory/issuance-form/resolvers';
+import IssuanceFormSchema from "./inventory/issuance-form/issuance-form.graphql";
+import IssuanceFormResolvers from "./inventory/issuance-form/resolvers";
 
-import ReturnFormSchema from './inventory/return-form/return-form.graphql';
-import ReturnFormResolvers from './inventory/return-form/resolvers';
-
-import PurchaseFormSchema from './inventory/purchase-form/purchase-form.graphql';
-import PurchaseFormResolvers from './inventory/purchase-form/resolvers';
+import PurchaseFormSchema from "./inventory/purchase-form/purchase-form.graphql";
+import PurchaseFormResolvers from "./inventory/purchase-form/resolvers";
 
 /**
  * Schema and Resolvers for the HR module
  */
-import FinancialAccountSchema from './accounts/financial-account/financial-account.graphql';
-import FinancialAccountResolvers from './accounts/financial-account/resolvers';
+import FinancialAccountSchema from "./accounts/financial-account/financial-account.graphql";
+import FinancialAccountResolvers from "./accounts/financial-account/resolvers";
 
 const typeDefs = [
   AttachmentSchema,
@@ -67,7 +64,6 @@ const typeDefs = [
   PhysicalStoreSchema,
   StockItemSchema,
   IssuanceFormSchema,
-  ReturnFormSchema,
   PurchaseFormSchema,
 
   FinancialAccountSchema,
@@ -87,7 +83,6 @@ const resolvers = merge(
   PhysicalStoreResolvers,
   StockItemResolvers,
   IssuanceFormResolvers,
-  ReturnFormResolvers,
   PurchaseFormResolvers,
 
   FinancialAccountResolvers

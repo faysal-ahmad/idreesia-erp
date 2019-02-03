533691df05a65b421e320017b5a47bd0a9b88c21
Faisal Ahmad <faisal.idreesi@gmail.com>
2019-02-03 15:52:37 +0500

    Composer for loading list of locations

--- idreesia-erp/imports/ui/modules/inventory/common/composers/index.js
+++ idreesia-erp/imports/ui/modules/inventory/common/composers/index.js
@@ -1,3 +1,6 @@
-export { default as WithKarkuns } from './with-karkuns';
-export { default as WithPhysicalStoreId } from './with-physical-store-id';
-export { default as WithStockItemsByPhysicalStore } from './with-stock-items-by-physical-store';
+export { default as WithKarkuns } from "./with-karkuns";
+export { default as WithLocations } from "./with-locations";
+export { default as WithPhysicalStoreId } from "./with-physical-store-id";
+export {
+  default as WithStockItemsByPhysicalStore,
+} from "./with-stock-items-by-physical-store";
---
+++ idreesia-erp/imports/ui/modules/inventory/common/composers/with-locations.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+
+export default () => WrappedComponent => {
+  const WithLocations = props => <WrappedComponent {...props} />;
+
+  WithLocations.propTypes = {
+    locationsListLoading: PropTypes.bool,
+    allLocations: PropTypes.array,
+  };
+
+  const locationsListQuery = gql`
+    query allLocations {
+      allLocations {
+        _id
+        name
+        description
+      }
+    }
+  `;
+
+  return graphql(locationsListQuery, {
+    props: ({ data }) => ({ locationsListLoading: data.loading, ...data }),
+  })(WithLocations);
+};

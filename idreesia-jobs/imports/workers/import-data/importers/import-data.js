d10c2ecbfd94caa9c3f09ab5ef1716a40e539c1f
Faisal Ahmad <faisal.idreesi@gmail.com>
2019-02-19 15:04:59 +0500

    Updated job to import data based on import type.

--- idreesia-jobs/imports/workers/import-data/importers/import-data.js
+++ idreesia-jobs/imports/workers/import-data/importers/import-data.js
@@ -7,10 +7,17 @@ import importCategoriesData from "./import-categories-data";
 export default async function importData(dataImport, company) {
   try {
     const adminUser = Accounts.findUserByUsername("erp-admin");
-    const importedCategories = await importCategoriesData(company, adminUser);
-    dataImport.logs.push(`Imported ${importedCategories} categories.`);
 
-    dataImport.status = "completed";
+    if (dataImport.importType === "categories") {
+      const importedCategoriesCount = await importCategoriesData(
+        company,
+        adminUser
+      );
+      dataImport.logs.push(`Imported ${importedCategoriesCount} categories.`);
+      dataImport.status = "completed";
+    } else if (dataImport.importType === "vouchers") {
+      // import vouchers here
+    }
   } catch (err) {
     dataImport.status = "errored";
     dataImport.errorDetails = err.message;

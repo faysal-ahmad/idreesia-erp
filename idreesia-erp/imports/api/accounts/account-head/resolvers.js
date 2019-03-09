f861489d6410ab9cfdd676a93356c746f7105e61
Faisal Ahmad <faisal.idreesi@gmail.com>
2019-03-09 08:36:24 +0500

    Mutation method implementation and apply permissions on account heads.

--- idreesia-erp/imports/api/accounts/account-head/resolvers.js
+++ idreesia-erp/imports/api/accounts/account-head/resolvers.js
@@ -1,4 +1,6 @@
 import { AccountHeads } from "meteor/idreesia-common/collections/accounts";
+import { hasOnePermission } from "/imports/api/security";
+import { Permissions as PermissionConstants } from "meteor/idreesia-common/constants";
 
 export default {
   AccountHead: {
@@ -12,11 +14,65 @@ export default {
     },
   },
   Query: {
-    accountHeadById(obj, { _id }) {
+    accountHeadById(obj, { _id }, { user }) {
+      if (
+        !hasOnePermission(user._id, [
+          PermissionConstants.ACCOUNTS_VIEW_ACCOUNT_HEADS,
+          PermissionConstants.ACCOUNTS_MANAGE_ACCOUNT_HEADS,
+        ])
+      ) {
+        return null;
+      }
+
       return AccountHeads.findOne({ _id });
     },
-    accountHeadsByCompanyId(obj, { companyId }) {
+    accountHeadsByCompanyId(obj, { companyId }, { user }) {
+      if (
+        !hasOnePermission(user._id, [
+          PermissionConstants.ACCOUNTS_VIEW_ACCOUNT_HEADS,
+          PermissionConstants.ACCOUNTS_MANAGE_ACCOUNT_HEADS,
+        ])
+      ) {
+        return [];
+      }
+
       return AccountHeads.find({ companyId: { $eq: companyId } }).fetch();
+    },
+  },
+
+  Mutation: {
+    updateAccountHead(
+      obj,
+      { _id, name, description, startingBalance },
+      { user }
+    ) {
+      if (
+        !hasOnePermission(user._id, [
+          PermissionConstants.ACCOUNTS_MANAGE_ACCOUNT_HEADS,
+        ])
+      ) {
+        throw new Error(
+          "You do not have permission to manage Account Heads in the System."
+        );
+      }
+
+      const date = new Date();
+      AccountHeads.update(
+        {
+          _id: { $eq: _id },
+        },
+        {
+          $set: {
+            name,
+            description,
+            startingBalance,
+            updatedAt: date,
+            updatedBy: user._id,
+          },
+        }
+      );
+
+      return AccountHeads.findOne(_id);
     },
   },
 };

fcd5b64f65d6b9d839dd948159bb4f7de96652d1
Faisal Ahmad <faisal.idreesi@gmail.com>
2019-04-14 22:55:55 +0500

    Minor refactoring

--- idreesia-erp/imports/ui/modules/helpers/controls/index.js
+++ idreesia-erp/imports/ui/modules/helpers/controls/index.js
@@ -1,12 +1,9 @@
+export { default as TakePicture } from "./picture/take-picture";
+export { default as UploadAttachment } from "./upload/upload-attachment";
 export { default as AttachmentsList } from "./attachments/attachments-list";
 
-export { default as CaptureBarcode } from "./barcode/capture-barcode";
-export { default as CaptureBarcodeForm } from "./barcode/capture-barcode-form";
+export { default as CaptureBarcode } from "./barcode/webcam/capture-barcode";
 export { default as DisplayBarcode } from "./barcode/display-barcode";
-
-export { default as TakePicture } from "./picture/take-picture";
-
-export { default as UploadAttachment } from "./upload/upload-attachment";
 
 export { getNameWithImageRenderer } from "./paged-data-list-renderers";
 export { default as PagedDataList } from "./paged-data-list";

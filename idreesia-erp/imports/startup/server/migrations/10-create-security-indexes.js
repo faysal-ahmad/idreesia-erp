e8b4ecadbcd17c5b2082ca5c606026689a82b0c9
Faisal Ahmad <faisal.idreesi@gmail.com>
2019-07-21 06:03:42 +0500

    Updated security indexes

--- idreesia-erp/imports/startup/server/migrations/10-create-security-indexes.js
+++ idreesia-erp/imports/startup/server/migrations/10-create-security-indexes.js
@@ -1,17 +1,28 @@
 import { Migrations } from "meteor/percolate:migrations";
 
-import { Visitors } from "meteor/idreesia-common/collections/security";
+import {
+  Visitors,
+  VisitorStays,
+} from "meteor/idreesia-common/collections/security";
 
 Migrations.add({
   version: 10,
   up() {
     const visitors = Visitors.rawCollection();
-    visitors.createIndex({
-      firstName: "text",
-      lastName: "text",
-    });
+    visitors.createIndex({ name: "text" });
     visitors.createIndex({ cnicNumber: 1 }, { background: true });
     visitors.createIndex({ contactNumber1: 1 }, { background: true });
     visitors.createIndex({ contactNumber2: 1 }, { background: true });
+    visitors.createIndex({ city: 1 }, { background: true });
+    visitors.createIndex({ country: 1 }, { background: true });
+
+    const visitorStays = VisitorStays.rawCollection();
+    visitorStays.createIndex({ visitorId: 1 }, { background: true });
+    visitorStays.createIndex({ fromDate: 1 }, { background: true });
+    visitorStays.createIndex({ toDate: 1 }, { background: true });
+    visitorStays.createIndex({ stayReason: 1 }, { background: true });
+    visitorStays.createIndex({ stayAllowedBy: 1 }, { background: true });
+    visitorStays.createIndex({ dutyId: 1 }, { background: true });
+    visitorStays.createIndex({ shiftId: 1 }, { background: true });
   },
 });

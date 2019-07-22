f66dee18bdcedc4f3acde621669946ed507d3f18
Faisal Ahmad <faisal.idreesi@gmail.com>
2019-07-22 09:35:00 +0500

    Fix url for images

--- idreesia-erp/imports/ui/modules/helpers/misc/get-download-url.js
+++ idreesia-erp/imports/ui/modules/helpers/misc/get-download-url.js
@@ -1,9 +1,4 @@
-// import { Meteor } from "meteor/meteor";
-
 export default function getDownloadUrl(attachmentId) {
   if (!attachmentId) return null;
-  // return Meteor.absoluteUrl(`download-file?attachmentId=${attachmentId}`);
-  return `${
-    window.location.hostname
-  }/download-file?attachmentId=${attachmentId}`;
+  return `${window.location.origin}/download-file?attachmentId=${attachmentId}`;
 }

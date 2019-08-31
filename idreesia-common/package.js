8573ab5e85791218bbeb7a5dae8fef35354220c5
Faisal Ahmad <faisal.idreesi@gmail.com>
2019-09-01 03:15:30 +0500

    Add required packages to use in the idreesia-common package.

--- idreesia-common/package.js
+++ idreesia-common/package.js
@@ -1,11 +1,14 @@
 Package.describe({
-  name: "idreesia-common",
-  version: "1.0.0",
-  summary: "Contains common code used by both the ERP and Jobs app",
+  name: 'idreesia-common',
+  version: '1.0.0',
+  summary: 'Contains common code used by both the ERP and Jobs app',
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom("1.8.0.1");
-  api.use("ecmascript");
-  api.use("swydo:graphql@0.4.0");
+Package.onUse(api => {
+  api.versionsFrom('1.8.0.1');
+  api.use('ecmascript');
+  api.use('aldeed:collection2-core');
+  api.use('swydo:graphql@0.4.0');
+  api.use('accounts-password@1.5.1');
+  api.use('apollo');
 });

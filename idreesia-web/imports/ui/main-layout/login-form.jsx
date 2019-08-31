40c3be1f1b59bea38163d1340aecadc306848dae
Faisal Ahmad <faisal.idreesi@gmail.com>
2019-09-01 04:09:47 +0500

    Added log to login form.

--- idreesia-web/imports/ui/main-layout/login-form.jsx
+++ idreesia-web/imports/ui/main-layout/login-form.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { Form, Icon, Input, Button, message } from "antd";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Form, Icon, Input, Button, message } from 'antd';
 
 const loginFormButtonStyle = {
-  width: "100%",
+  width: '100%',
 };
 
 class LoginForm extends Component {
@@ -22,6 +22,8 @@ class LoginForm extends Component {
         Meteor.loginWithPassword(userName, password, error => {
           if (!error) {
             history.push(location.pathname);
+            // eslint-disable-next-line no-console
+            console.log(`Login successful. Routing to location.pathname`);
           } else {
             message.error(error.message, 5);
           }
@@ -31,34 +33,38 @@ class LoginForm extends Component {
   };
 
   getUserNameField() {
-    const { form : { getFieldDecorator } } = this.props;
+    const {
+      form: { getFieldDecorator },
+    } = this.props;
     const rules = [
       {
         required: true,
-        message: "Please input your username.",
+        message: 'Please input your username.',
       },
     ];
-    return getFieldDecorator("userName", { rules })(
+    return getFieldDecorator('userName', { rules })(
       <Input
         placeholder="Username"
-        prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+        prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
       />
     );
   }
 
   getPasswordField() {
-    const { form : { getFieldDecorator } } = this.props;
+    const {
+      form: { getFieldDecorator },
+    } = this.props;
     const rules = [
       {
         required: true,
-        message: "Please input your password.",
+        message: 'Please input your password.',
       },
     ];
-    return getFieldDecorator("password", { rules })(
+    return getFieldDecorator('password', { rules })(
       <Input
         type="password"
         placeholder="Password"
-        prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
       />
     );
   }

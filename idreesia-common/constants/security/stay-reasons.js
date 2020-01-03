e0c74d62a73969968727c7ccb17cd0bd8707d0b8
Faisal Ahmad <faisal.idreesi@gmail.com>
2020-01-03 18:53:26 +0500

    Added more options for stay reason for visitors

--- idreesia-common/constants/security/stay-reasons.js
+++ idreesia-common/constants/security/stay-reasons.js
@@ -1,9 +1,12 @@
 const stayReasons = [
   { _id: 'nishast-duty', name: 'Nishast Duty' },
+  { _id: 'nishast-duty-co', name: 'Nishast Duty (CO)' },
   { _id: 'security-duty', name: 'Security Duty' },
-  { _id: 'construction-duty', name: 'Construction Duty' },
-  { _id: 'waiting-for-response', name: 'Waiting for Letter Response' },
+  { _id: 'security-duty-co', name: 'Security Duty (CO)' },
   { _id: 'team-visit', name: 'Team Visit' },
+  { _id: 'team-visit-co', name: 'Team Visit (CO)' },
+  { _id: 'waiting-for-response', name: 'Waiting for Letter Response' },
+  { _id: 'construction-duty', name: 'Construction Duty' },
 ];
 
 export default stayReasons;

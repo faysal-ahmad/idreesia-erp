bf68e195b5afb0b541b50a68deaad7f830d7e6d3
Farooq Hameed <virtualit786@gmail.com>
2020-01-07 18:17:48 +0500

    Fixed #7 Salaries pagination

--- idreesia-common/server/graphql-api/hr/salary/resolvers.js
+++ idreesia-common/server/graphql-api/hr/salary/resolvers.js
@@ -11,6 +11,8 @@ import {
   Permissions as PermissionConstants,
 } from 'meteor/idreesia-common/constants';
 import { createMonthlySalaries } from 'meteor/idreesia-common/server/business-logic/hr/create-monthly-salaries';
+import { parse } from 'query-string';
+import { get } from 'meteor/idreesia-common/utilities/lodash';
 
 export default {
   SalaryType: {
@@ -94,6 +96,42 @@ export default {
       return Salaries.find({
         _id: { $in: idsArray },
       }).fetch();
+    },
+
+    pagedSalariesByKarkun(obj, { queryString }) {
+      console.log(
+        '/hr/salary/resolvers.js/resolvers.js::pagedSalariesByKarkun',
+        queryString
+      );
+      const params = parse(queryString);
+      const pipeline = [];
+      const { pageIndex = '0', pageSize = '20', karkunId } = params;
+
+      pipeline.push({
+        $match: {
+          karkunId: { $eq: karkunId },
+        },
+      });
+
+      const countingPipeline = pipeline.concat({
+        $count: 'total',
+      });
+
+      const nPageIndex = parseInt(pageIndex, 10);
+      const nPageSize = parseInt(pageSize, 10);
+      const resultsPipeline = pipeline.concat([
+        { $sort: { createdAt: -1 } },
+        { $skip: nPageIndex * nPageSize },
+        { $limit: nPageSize },
+      ]);
+
+      const karkunSalaries = Salaries.aggregate(resultsPipeline).toArray();
+      const totalResults = Salaries.aggregate(countingPipeline).toArray();
+
+      return Promise.all([karkunSalaries, totalResults]).then(results => ({
+        salaries: results[0],
+        totalResults: get(results[1], ['0', 'total'], 0),
+      }));
     },
   },
 

71140575b7bc05ef3475aa4a7d2a2a374ff88592
Farooq Hameed <virtualit786@gmail.com>
2020-01-07 11:29:35 +0500

    Fixed #6 Integrated gitflow using git tools. https://datasift.github.io/gitflow/GitFlowForGitHub.html

--- idreesia-common/server/graphql-api/hr/attendance/resolvers.js
+++ idreesia-common/server/graphql-api/hr/attendance/resolvers.js
@@ -17,6 +17,8 @@ import {
 } from 'meteor/idreesia-common/constants';
 import { createMonthlyAttendance } from 'meteor/idreesia-common/server/business-logic/hr/create-monthly-attendance';
 import { processAttendanceSheet } from './helpers';
+import { parse } from 'query-string';
+import { get } from 'meteor/idreesia-common/utilities/lodash';
 
 export default {
   AttendanceType: {
@@ -77,7 +79,39 @@ export default {
         { sort: { createdAt: -1 } }
       ).fetch();
     },
+    pagedAttendanceByKarkun(obj, { queryString }) {
+      const params = parse(queryString);
+      const pipeline = [];
+      const { pageIndex = '0', pageSize = '20', karkunId } = params;
 
+      pipeline.push({
+        $match: {
+          karkunId: { $eq: karkunId },
+        },
+      });
+
+      const countingPipeline = pipeline.concat({
+        $count: 'total',
+      });
+
+      const nPageIndex = parseInt(pageIndex, 10);
+      const nPageSize = parseInt(pageSize, 10);
+      const resultsPipeline = pipeline.concat([
+        { $sort: { createdAt: -1 } },
+        { $skip: nPageIndex * nPageSize },
+        { $limit: nPageSize },
+      ]);
+
+      const karkunAttendences = Attendances.aggregate(
+        resultsPipeline
+      ).toArray();
+      const totalResults = Attendances.aggregate(countingPipeline).toArray();
+
+      return Promise.all([karkunAttendences, totalResults]).then(results => ({
+        attendance: results[0],
+        totalResults: get(results[1], ['0', 'total'], 0),
+      }));
+    },
     attendanceByMonth(obj, { month, categoryId, subCategoryId }, { user }) {
       if (!categoryId) return [];
 

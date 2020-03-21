4a698e415f246e8659274c463f2073dcc819cef2
Faisal Ahmad <faisal.idreesi@gmail.com>
2020-03-21 06:47:59 +0500

    Upgrade to meteor 1.10.1

--- idreesia-common/package.js
+++ idreesia-common/package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'idreesia-common',
   version: '1.0.0',
-  summary: 'Contains common code used by both the ERP and Jobs app',
+  summary: 'Contains common code used by both the Web, Mobile and Jobs app',
 });
 
 Package.onUse(api => {
   api.addFiles('private/auth/google.json', 'server', { isAsset: true });
 
-  api.versionsFrom('1.9.2');
+  api.versionsFrom('1.10.1');
   api.use('ecmascript');
-  api.use('accounts-password@1.5.2');
+  api.use('accounts-password');
   api.use('aldeed:collection2-core', 'server');
-  api.use('swydo:graphql@0.4.0', 'server');
+  api.use('swydo:graphql', 'server');
 });

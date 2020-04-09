b58f425b7b5c405fd27dcfb147c26e3d74f82c41
Faisal Ahmad <faisal.idreesi@gmail.com>
2020-04-09 12:09:02 +0500

    Updated telephone room module to use directive based permissions checking

--- idreesia-common/server/graphql-api/telephone-room/imdad-request/resolvers.js
+++ idreesia-common/server/graphql-api/telephone-room/imdad-request/resolvers.js
@@ -1,39 +1,17 @@
 import moment from 'moment';
 import { ImdadRequests } from 'meteor/idreesia-common/server/collections/imdad';
-import { hasOnePermission } from 'meteor/idreesia-common/server/graphql-api/security';
-import { Permissions as PermissionConstants } from 'meteor/idreesia-common/constants';
 import { DataSource } from 'meteor/idreesia-common/constants';
 import { RequestStatus } from 'meteor/idreesia-common/constants/imdad';
 
 export default {
   Query: {
-    pagedTelephoneRoomImdadRequests(obj, { filter }, { user }) {
-      if (
-        !hasOnePermission(user._id, [
-          PermissionConstants.TR_VIEW_VISITORS,
-          PermissionConstants.TR_MANAGE_VISITORS,
-        ])
-      ) {
-        return {
-          data: [],
-          totalResults: 0,
-        };
-      }
-
+    pagedTelephoneRoomImdadRequests(obj, { filter }) {
       return ImdadRequests.getPagedData(filter);
     },
   },
 
   Mutation: {
     createTelephoneRoomImdadRequest(obj, { visitorId }, { user }) {
-      if (
-        !hasOnePermission(user._id, [PermissionConstants.TR_MANAGE_VISITORS])
-      ) {
-        throw new Error(
-          'You do not have permission to manage Imdad Requests in the System.'
-        );
-      }
-
       if (!ImdadRequests.isImdadRequestAllowed(visitorId)) {
         throw new Error(
           'Visitor already has submitted an imdad request in the last 30 days.'
@@ -57,13 +35,7 @@ export default {
       return ImdadRequests.findOne(imdadRequestId);
     },
 
-    deleteTelephoneRoomImdadRequest(obj, { _id }, { user }) {
-      if (!hasOnePermission(user._id, [PermissionConstants.TR_DELETE_DATA])) {
-        throw new Error(
-          'You do not have permission to delete Imdad Requests in the System.'
-        );
-      }
-
+    deleteTelephoneRoomImdadRequest(obj, { _id }) {
       return ImdadRequests.remove(_id);
     },
   },

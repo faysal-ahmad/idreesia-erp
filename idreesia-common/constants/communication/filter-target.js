d82716b48a5b31e318ad68ff00805995f1eeebcc
Faisal Ahmad <faisal.idreesi@gmail.com>
2020-09-17 10:12:04 +0500

    Removed unused constant value

--- idreesia-common/constants/communication/filter-target.js
+++ idreesia-common/constants/communication/filter-target.js
@@ -2,7 +2,6 @@ const FilterTarget = {
   MS_KARKUNS: 'ms-karkuns',
   OUTSTATION_KARKUNS: 'outstation-karkuns',
   VISITORS: 'visitors',
-  USER_ACCOUNTS: 'user-accounts',
 };
 
 export default FilterTarget;

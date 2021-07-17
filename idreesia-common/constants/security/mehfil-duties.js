b04ab2787fa9618f451eb865026db60174644bb1
Faisal Ahmad <faisal.idreesi@gmail.com>
2021-07-18 01:00:55 +0500

    Updated duty names to include current year

--- idreesia-common/constants/security/mehfil-duties.js
+++ idreesia-common/constants/security/mehfil-duties.js
@@ -1,7 +1,7 @@
 const MehfilDuties = [
   {
     _id: 'special-security',
-    name: 'Special Security',
+    name: 'Special Security 2021',
     urduName: 'اسپیشل سیکورٹی',
   },
   {
@@ -9,15 +9,31 @@ const MehfilDuties = [
     name: 'Entry Gate Security',
     urduName: 'اینٹری گیٹ سیکورٹی',
   },
-  { _id: 'hall-security', name: 'Hall Security', urduName: 'ہال سیکورٹی' },
-  { _id: '802-security', name: '802 Security', urduName: '٨٠٢ سیکورٹی' },
-  { _id: 'mehfil-ziyarat', name: 'Mehfil Ziyarat', urduName: 'محفل زیارت' },
-  { _id: 'inercom-duty', name: 'Intercom Duty', urduName: 'انٹرکام ڈیوٹی' },
-  { _id: 'telephone-room', name: 'Telephone Room', urduName: 'ٹیلی فون روم' },
-  { _id: 'joota-stand', name: 'Shoe Rack', urduName: 'جوتا اسٹینڈ' },
-  { _id: 'madad-committee', name: 'Madad Committee', urduName: 'مدد کمیٹی' },
-  { _id: 'wazaif-room', name: 'Wazaif Room', urduName: 'وظائف روم' },
-  { _id: 'qurbani', name: 'Qurbani Duty', urduName: 'قربانی ڈیوٹی' },
+  { _id: 'hall-security', name: 'Hall Security 2021', urduName: 'ہال سیکورٹی' },
+  { _id: '802-security', name: '802 Security 2021', urduName: '٨٠٢ سیکورٹی' },
+  {
+    _id: 'mehfil-ziyarat',
+    name: 'Mehfil Ziyarat 2021',
+    urduName: 'محفل زیارت',
+  },
+  {
+    _id: 'inercom-duty',
+    name: 'Intercom Duty 2021',
+    urduName: 'انٹرکام ڈیوٹی',
+  },
+  {
+    _id: 'telephone-room',
+    name: 'Telephone Room 2021',
+    urduName: 'ٹیلی فون روم',
+  },
+  { _id: 'joota-stand', name: 'Shoe Rack 2021', urduName: 'جوتا اسٹینڈ' },
+  {
+    _id: 'madad-committee',
+    name: 'Madad Committee 2021',
+    urduName: 'مدد کمیٹی',
+  },
+  { _id: 'wazaif-room', name: 'Wazaif Room 2021', urduName: 'وظائف روم' },
+  { _id: 'qurbani', name: 'Qurbani Duty 2021', urduName: 'قربانی ڈیوٹی' },
 ];
 
 export default MehfilDuties;
